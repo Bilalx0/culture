cb5c21df16b1a461928cd6865cf0ce913f084c24
Muhammad Bilal <muhammadbilaal098@gmail.com>
2025-02-16 15:53:12 +0500

    Initial commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className='bg-rose-950 font-nastaliq px-16 mt-8'>
+        <ul className='flex justify-between items-center h-14'>
+            <li className='flex justify-between items-center gap-24 text-xl text-orange-50'>
+                <span> پښتونخوا</span>
+                <span>بلوچستان</span>
+            </li>
+            <li>
+                <span></span>
+            </li>
+            <li className='flex justify-between items-center gap-24 text-xl text-orange-50'>
+                <span>سنڌ</span>
+                <span>پنجاب</span>
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
